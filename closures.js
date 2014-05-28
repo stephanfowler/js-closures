c3b290f8f171486986259185d8ba4bf293116039
Stephan Fowler <stephanfowler@gmail.com>
2014-05-28 20:34:01 +0100

    Just to settle this once and for all

---
+++ closures.js
@@ -0,0 +1,50 @@
+var fns = {},
+    i;
+
+function execAll(obj) {
+    for (var fn in obj) {
+        obj[fn]();
+    }
+}
+
+// Bad. Outputs 3 3 3
+for (i = 0; i < 3; i += 1) {
+    fns[i] = function() {
+        console.log(i);
+    };
+} 
+execAll(fns)
+
+// Bad. Outputs 3 3 3
+for (i = 0; i < 3; i += 1) {
+    fns[i] = function() {
+        var c = i;
+        console.log(c);
+    };
+} 
+execAll(fns)
+
+// Good. Outputs 0 1 2
+for (i = 0; i < 3; i += 1) {
+    fns[i] = (function(c) { 
+        return function() { console.log(c); }; 
+    })(i)
+} 
+execAll(fns)
+
+// Better. Outputs 0 1 2
+for (i = 0; i < 3; i += 1) {
+    fns[i] = closeOver(i);
+} 
+function closeOver(c) { 
+    return function() { console.log(c); }; 
+}
+execAll(fns)
+
+// Best. Outputs 0 1 2
+[0,1,2].forEach(function(i) {
+    fns[i] = function() { console.log(i); };
+});
+execAll(fns)
+
+
